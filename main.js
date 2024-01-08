594fc255c8306881870d3427688b3541404a43bc
heejin <dlwlrgkwk82@gmail.com>
2024-01-08 18:24:51 +0900

    feat: add gradiant

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let container = document.querySelector(".container");
+let overlay = document.querySelector(".overlay");
 container.addEventListener("mousemove", function (e) {
   let x = e.offsetX;
   let y = e.offsetY;
   let rotateY = (-1 / 5) * x + 20;
   let rotateX = (4 / 30) * y - 20;
+
+  overlay.style = `background-position: ${x / 5 + y / 5}%`;
+
   container.style = `transform: perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
